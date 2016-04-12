1129f4a26141f4da164b3412530e348b4c4bc464
Ramu reddy <ramureddy@Ramus-MacBook-Pro.local>
2016-04-11 21:05:09 -0400

    adding files superone

--- superone/ViewController.swift
+++ superone/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var cool: UIImageView!
+    
+    @IBOutlet weak var butto: UIButton!
+
+   
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func butto(sender: AnyObject) {
+        cool.hidden=false
+        butto.hidden=true
     }
 
 
